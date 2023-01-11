2b5281aa0cd471cbb366bbb98c6265b0a36e474a
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-01-10 21:29:15 -0300

    Add Jump and Friction

--- Assets/Scripts/Prototype/Player.cs
+++ Assets/Scripts/Prototype/Player.cs
@@ -6,9 +6,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody2D;
-    public Vector2 velocity;
+    public Vector2 friction;
     public float speed;
+    public float jumpForce;
+
+    private void Awake()
+    {
+        jumpForce = 15f;
+        friction = new Vector2(0.1f, 0);
+    }
+
     private void Update()
+    {
+        PlayerJump();
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -17,6 +31,23 @@ public class Player : MonoBehaviour
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             myRigidbody2D.velocity = new Vector2(speed,myRigidbody2D.velocity.y);
+        }
+
+        if (myRigidbody2D.velocity.x > 0)
+        {
+            myRigidbody2D.velocity -= friction;
+        }
+        else if (myRigidbody2D.velocity.x < 0)
+        {
+            myRigidbody2D.velocity += friction;
+        }
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * jumpForce;
         }
     }
 }
