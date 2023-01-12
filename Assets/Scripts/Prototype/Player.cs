fb3feb8f05b829a0720df27098eb0de2cb545494
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-01-11 21:32:20 -0300

    Cinemachine and Jump Scale

--- Assets/Scripts/Prototype/Player.cs
+++ Assets/Scripts/Prototype/Player.cs
@@ -2,23 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody2D;
-    public Vector2 friction;
-    public float speed;
-    public float speedRun;
-    public float jumpForce;
-
+    [Header("References")]
+        public Rigidbody2D myRigidbody2D;
+        public Vector2 friction;
+    [Header("Move and Jump")]
+        public float speed;
+        public float speedRun;
+        public float jumpForce;
+    [Header("Animation")] 
+        public float jumpScaleY = 1.5f;
+        public float jumpScaleX = 0.7f;
+        public float animationDuration = 0.3f;
+        public Ease ease;
+    
+        
     private float _currentSpeed;
 
     private void Awake()
     {
         jumpForce = 15f;
         speed = 5f;
-        speedRun = 15f;
+        speedRun = 10f;
         friction = new Vector2(0.1f, 0);
+        ease = Ease.OutBack;
     }
 
     private void Update()
@@ -49,10 +59,10 @@ public class Player : MonoBehaviour
             switch (myRigidbody2D.velocity.x)
             {
                 case < 0: 
-                    myRigidbody2D.velocity -= friction;
+                    myRigidbody2D.velocity += friction;
                     break; 
                 case > 0: 
-                    myRigidbody2D.velocity += friction;
+                    myRigidbody2D.velocity -= friction;
                     break;
             }
 
@@ -65,6 +75,21 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody2D.velocity = Vector2.up * jumpForce;
+            
+            var rigidbody2DTransform = myRigidbody2D.transform;
+            rigidbody2DTransform.localScale = Vector2.one;
+            DOTween.Kill(rigidbody2DTransform);
+            
+            PlayerJumpScale();
         }
+    }
+
+    private void PlayerJumpScale()
+    {
+        /*fazer scale quando o player cai*/
+        myRigidbody2D.transform.DOScaleY(jumpScaleY,animationDuration).SetLoops(2,
+            LoopType.Yoyo).SetEase(ease);
+        myRigidbody2D.transform.DOScaleX(jumpScaleX,animationDuration).SetLoops(2,
+            LoopType.Yoyo).SetEase(ease);
     }
 }
