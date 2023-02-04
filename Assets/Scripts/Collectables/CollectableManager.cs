7fd9138e4388d522290e0bb565a6de656ce149e1
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-02-04 17:03:48 -0300

    Coins In HUD

--- Assets/Scripts/Collectables/CollectableManager.cs
+++ Assets/Scripts/Collectables/CollectableManager.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Core.Singleton;
+using TMPro;
+
 public class CollectableManager : Singleton<CollectableManager>
 {
     public int coins;
+    public TextMeshProUGUI coinsInHud;
     private void Start()
     {
         Reset();
@@ -14,6 +17,11 @@ public class CollectableManager : Singleton<CollectableManager>
     private void Reset()
     {
         coins = 0;
+    }
+
+    private void Update()
+    {
+        coinsInHud.text = "x " + coins.ToString();
     }
 
     public void AddCoins(int amount = 1)
